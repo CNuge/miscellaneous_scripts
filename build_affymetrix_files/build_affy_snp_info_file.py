867745e0f8df11ba816324b751ccbcdf0a2ab35e
WATTBA <nugentc@uoguelph.ca>
2017-07-12 12:20:21 -0400

    building the .tsv from the df

--- build_affymetrix_files/build_affy_snp_info_file.py
+++ build_affymetrix_files/build_affy_snp_info_file.py
@@ -1,3 +1,4 @@
+import argparse 
 import pandas as pd
 from pandas import Series, DataFrame
 
@@ -15,7 +16,7 @@ def get_71mer(contig, pos, alleles):
 		35 bases either side of the snp, insert the alleles in the middle of 
 		the sequence, in alphabetical order """
 	try:
-		contig[pos-36] #check if snp is to close to the end of the contigs
+		contig[pos-36] #check if snp is too close to the end of the contigs
 		contig[pos+35]
 	except:
 		return '-' #if it is, return a blank 71mer so we know to exclude
@@ -28,7 +29,7 @@ def get_71mer(contig, pos, alleles):
 	return out_dat
 
 def load_contigs(contig_fasta_file):
-	"""load the contigs into memory, in order ot get surrounding bp """
+	"""load the contigs into memory, in order to get surrounding bp """
 	contig_dict = {}
 	with open(contig_fasta_file) as file:
 		for line in file:
@@ -43,7 +44,7 @@ def read_vcf(filename):
 		for line in file:
 			if line[:2] == "##":
 				continue
-			elif line[0] = "#"
+			elif line[0] == "#":
 				header = line[1:].rstrip().split('\t')
 			else:
 				line_dat = line.rstrip().split('\t')
@@ -51,32 +52,67 @@ def read_vcf(filename):
 	vcf_df = DataFrame(data = in_data, columns = header)
 	return vcf_df
 
+def name_split(name):
+	""" return only first segment of a name with underscores in it """
+	new = name.split('_')[0]
+	return new
 
 if __name__ == '__main__':
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input_vcf", help="input the .vcf file")
+parser.add_argument("-c", "--contigs", type=str, help="input the genome in .fasta fmt")
+parser.add_argument("-p", "--priority_level", type=str, help="state the priority level of this snp batch")
+parser.add_argument("-s", "--strain", type=str, help="state the strain of origin of this snp batch")
+parser.add_argument("-o", "--organism", type=str, help="state the name of the species")
+args = parser.parse_args()
+
+
+
 """ read in the genome fasta  """
-contig_dict = load_contigs('salp_contig_file.fasta')
+contig_dict = load_contigs('salp_contig_file.fasta') #change to args.contigs
 
 """ read the .vcf in as a dataframe, then project get_71mer through a lambda """
-""" remove all the top lines with # except for the header, just easier this way """
-snp_data = read_vcf('fraser_strain_snps_one_location.vcf')
+snp_data = read_vcf('fraser_strain_snps_one_location.vcf') #change to args.input_vcf
 
 """ apply the 71mer build to the dataframe to make a new column 
 	note it calls the contig in the dictonary to get the sequence """
-snp_data['seventyone_mer'] = snp_data.apply(
-		lambda x: get_71mer(contig_dict[x['#CHROM']], x['Pos'], [x['REF'],x['ALT']]))
+snp_data['seventyonemer'] = snp_data.apply(
+		lambda x: get_71mer(contig_dict[x['CHROM']], x['Pos'], [x['REF'],x['ALT']]), axis=1)
 
-""" add an argparse for the 1. snp_priority level 2. strain of origin
-	add this information to the dataframe """
+""" change the name of the snps, store in affy column name"""
+snp_data['snpid'] = snp_data.apply(
+		lambda x: name_split(x['ID']), axis=1)
+
+""" add this information to the dataframe:
+	1. snp_priority level 
+	2. strain of origin """
+
+snp_data['SNP_PRIORITY'] = args.priority_level
+
+snp_data['REF_STR'] = args.strain
+
+snp_data['Organism'] = args.organism
 
 """next reorder and rename the columns to the affymetrix specs, then output the data"""
 
+snp_data['CHR'] = snp_data['CHROM']
+
+snp_data['SNP_VAL'] = '0'
+snp_data['CHR_TYPE'] = 'autosomal'
+snp_data['seventyonemer']= 'placeholder'
 
 
+""" list the columns needed in the affymetrix output file """
+final_cols = ['Organism','snpid','REF_STR','seventyonemer','CHR','POS','SNP_PRIORITY','SNP_VAL','CHR_TYPE']
 
+""" make a subset df, output to a tab delimited file """
+output_data = snp_data[final_cols]
 
+output_name =  args.vcf.split('.')[0] + ".tsv"
 
-
+output_data.to_csv(output_name, sep='\t', index=False)
 
 
 
