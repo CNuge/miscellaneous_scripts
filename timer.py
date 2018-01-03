bb75891dc667019ef5d422ce101c26bc83dfd82d
WATTBA <nugentc@uoguelph.ca>
2018-01-03 15:11:50 -0500

    warts

--- timer.py
+++ timer.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 import time
 
+
 def clock(func):
-    def clocked(*args): #any number of positional arguments
+    def clocked(*args, **kwargs): #any number of positional arguments
         t0 = time.perf_counter() #performance counter returns a float time in seconds
-        result = func(*args)
-        t1 = time.perf_counter()
+        result = func(*args, **kwargs)
+        elapsed = time.perf_counter() - t0
         name = func.__name__
         arg_str =' ,'.join(repr(arg) for arg in args)
-        print('function %s ran in: %0.8fs' % (name, t1 - t0))
+        print('function %s ran in: %0.8fs' % (name, elapsed ))
         return result
     return clocked #return the iner function to replace the decorated func
+
