e490ca1a68b7fac30b8619f0d05de1cf2e7c590f
WATTBA <nugentc@uoguelph.ca>
2018-01-03 14:48:13 -0500

    delta

--- timer.py
+++ timer.py
@@ -5,10 +5,9 @@ def clock(func):
     def clocked(*args): #any number of positional arguments
         t0 = time.perf_counter() #performance counter returns a float time in seconds
         result = func(*args)
-        elapsed = time.perf_counter()
+        t1 = time.perf_counter()
         name = func.__name__
         arg_str =' ,'.join(repr(arg) for arg in args)
-        print('function %s ran in: %0.8fs' % (name, elapsed))
+        print('function %s ran in: %0.8fs' % (name, t1 - t0))
         return result
     return clocked #return the iner function to replace the decorated func
-
